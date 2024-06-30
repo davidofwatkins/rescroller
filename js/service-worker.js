92869a43b69048f29e95f912d3dfb7e514687363
David Watkins <david@davidofwatkins.com>
2024-06-30 13:55:19 -0700

    Refactor service worker using new utils

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,4 +1,21 @@
-import { Rescroller } from './rescroller.js';
+import {
+  generateScrollbarCSS,
+  setFirstTimeData,
+  getListOfDisabledSites,
+  RESCROLLER_SETTINGS_KEY,
+} from './utils/index.js';
+import { getChromeStorageValue } from './utils/storage.js';
+
+// Keep track of disabled sites in memory
+let disabledSites = [];
+
+// We can't check the tab URL without the "tabs" (or "activeTab") permission, so
+// we need to catch errors like these after attempting to inject CSS
+const isExpectedChromeError = (e) =>
+  e.message.includes('Cannot access a chrome:// URL') ||
+  e.message.includes(
+    'Extension manifest must request permission to access the respective host'
+  );
 
 /**
  * Background Event page: this handles events for the extension between pages and performs the following:
@@ -15,54 +32,58 @@ import { Rescroller } from './rescroller.js';
  * hasn't been precomputed or needs to be regenerated.
  */
 
-chrome.runtime.onInstalled.addListener((details) => {
-  // when extension installed/updated and chrome updated
+chrome.runtime.onInstalled.addListener(async ({ reason }) => {
+  const existingSettings = await getChromeStorageValue(RESCROLLER_SETTINGS_KEY);
 
-  /**
-   * If this is the first time running the extension, open options page
-   * and Download latest Chrome Storage to Local Storage.
-   */
+  // If no existing data, assume we've just installed
+  if (!existingSettings || !Object.keys(existingSettings).length) {
+    await setFirstTimeData();
 
-  if (details.reason !== 'install') {
-    // no need to sync down if updating.
-    return;
+    if (reason === 'install') {
+      // ooen settings page only if not an update
+      chrome.tabs.create({ url: 'options.html' });
+    }
+  } else {
+    // (Re-)generate CSS in case this is a fresh install on a new device
+    await generateScrollbarCSS(existingSettings);
   }
-
-  Rescroller.syncDown(() => {
-    if (localStorage.install_time) {
-      return;
-    } // only open options.html if this is the first install on any of the users' Chromes
-
-    localStorage.install_time = new Date().getTime();
-    chrome.tabs.create({ url: 'options.html' });
-  });
 });
 
 /**
  * Listen for page states to change and set our custom CSS when tabs are loading
  */
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status !== 'loading') {
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (changeInfo.status !== 'complete') {
     return;
   }
 
-  // Check to make sure this url isn't blacklisted. Exit early if so
-  const disabledSites = Rescroller.getListOfDisabledSites();
-
-  // @todo this could probably be more accurate
-  const isDisabledSite = disabledSites.some((site) => tab.url.includes(site));
+  const isDisabledSite = disabledSites.some((site) => tab?.url?.includes(site));
 
   if (isDisabledSite) {
     return;
   }
 
-  // Aaaand, inject our customized CSS into the webpage!
-  chrome.tabs.insertCSS(tabId, {
-    // unfortunately, this requires the <all_urls> permission :/
-    code: localStorage['generated-css'],
-    allFrames: true,
-    runAt: 'document_start',
-  });
+  const generatedCss = await getChromeStorageValue('generated-css', 'local');
+
+  if (!generatedCss) {
+    return;
+  }
+
+  try {
+    await chrome.scripting.insertCSS({
+      css: generatedCss,
+      target: {
+        allFrames: true,
+        tabId,
+      },
+      origin: 'AUTHOR',
+    });
+  } catch (e) {
+    if (isExpectedChromeError(e)) {
+      return;
+    }
+    console.error('Failed to inject CSS:', e);
+  }
 });
 
 /**
@@ -72,18 +93,14 @@ chrome.action.onClicked.addListener(() => {
   chrome.tabs.create({ url: 'options.html' });
 });
 
-/**
- * Detect changes to Chrome Storage, and import them into Local Storage
- */
-chrome.storage.onChanged.addListener((changes) => {
-  // Convert these 'changes' into a key-val object, parallel to how localStorage is formatted (no need for change[].oldValue)
-  const changesCleaned = Object.keys(changes).reduce(
-    (accumulator, key) => ({
-      ...accumulator,
-      [key]: changes[key].newValue,
-    }),
-    {}
-  );
+chrome.storage.onChanged.addListener(async (changes, areaName) => {
+  disabledSites = await getListOfDisabledSites();
 
-  Rescroller.mergeSyncWithLocalStorage(changesCleaned);
+  // Update scrollbars when another device has updated them
+  if (areaName === 'sync') {
+    const existingSettings = await getChromeStorageValue(
+      RESCROLLER_SETTINGS_KEY
+    );
+    await generateScrollbarCSS(existingSettings);
+  }
 });
